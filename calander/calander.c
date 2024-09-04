fa5de9ee3ed9602e4f8ad1eda37a2afe705970aa
Nate Eldering <elderingn@vcu.edu>
2024-09-03 21:58:16 -0400

    updated calander.c

--- calander/calander.c
+++ calander/calander.c
@@ -2,14 +2,6 @@
 #include <stdbool.h>
 int days_in_month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-void add_days_to_date(int *mm, int *dd, int *yy, int days_left_to_add){
-  int days_left_in_month;
-  while(days_left_to_add > 0){
-    days_left_in_month = days_in_month[*mm] - *dd;
-    
-  }
-}
-
 bool is_leap_year(int year){
   if(year % 4 != 0){
     return false;
@@ -29,14 +21,46 @@ bool is_leap_year(int year){
   }
 }
 
+void add_days_to_date(int *mm, int *dd, int *yy, int days_left_to_add) {
+    while (days_left_to_add > 0) {
+        // Adjust for leap year in February
+        if (*mm == 2 && is_leap_year(*yy)) {
+            days_in_month[2] = 29;
+        } else {
+            days_in_month[2] = 28;
+        }
+
+        int days_left_in_month = days_in_month[*mm] - *dd;
+
+        if (days_left_to_add > days_left_in_month) {
+            // Move to the next month
+            days_left_to_add -= (days_left_in_month + 1);
+            *dd = 1;
+            *mm += 1;
+
+            // If the month goes beyond December, move to the next year
+            if (*mm > 12) {
+                *mm = 1;
+                *yy += 1;
+            }
+        } else {
+            *dd += days_left_to_add;
+            days_left_to_add = 0;
+        }
+    }
+}
+
 int main() {
-  int year;
-  printf("enter year between 1800 and 10000 \n");
-  scanf("%d", &year);
-  if(is_leap_year(year)){
-    printf("%d is a leap year \n", year);
-  }
-  else{
-    printf("%d is not a leap year \n", year);
-  }
+  int mm;
+  int dd;
+  int yy;
+  int days_to_add;
+  printf("enter month day and year in form mm dd yy \n");
+  scanf("%d" "%d" "%d", &mm, &dd, &yy);
+  printf("enter number of days to add \n");
+  scanf("%d", days_to_add);
+
+  add_days_to_date(&mm, &dd, &yy, days_to_add);
+
+  printf("%d %d %d", mm, dd, yy); 
 }
